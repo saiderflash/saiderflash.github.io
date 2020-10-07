a6491f014f224de9f999a4ff09e76a00f9e9bb27
saiderflash <saiderflash@gmail.com>
2020-10-07 17:18:33 +0630

    Updates

---
+++ precache-manifest.7a3a9e4080d7ec1461abb2548c66c763.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e901b55c6f28474d74e54bacd9a8e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "890b14ca767133da6aa7",
+    "url": "/static/css/3.9998556b.chunk.css"
+  },
+  {
+    "revision": "a44bb3028bf1ba6103e0",
+    "url": "/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "6978836cd3e1b77b5f8f",
+    "url": "/static/css/main.85145379.chunk.css"
+  },
+  {
+    "revision": "47b178f9b1f52da63bc1",
+    "url": "/static/js/2.3fa1a353.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.3fa1a353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890b14ca767133da6aa7",
+    "url": "/static/js/3.22c365f9.chunk.js"
+  },
+  {
+    "revision": "a44bb3028bf1ba6103e0",
+    "url": "/static/js/4.c7fcade5.chunk.js"
+  },
+  {
+    "revision": "6978836cd3e1b77b5f8f",
+    "url": "/static/js/main.1e64fc38.chunk.js"
+  },
+  {
+    "revision": "6de67b5dd3a15c05b4cd",
+    "url": "/static/js/runtime-main.55eef56b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "6d83c74fff84f6e50786d049bb4671a2",
+    "url": "/static/media/saiderlogo.6d83c74f.png"
+  },
+  {
+    "revision": "4f8e894296fd574fff68c35a93cd60e4",
+    "url": "/static/media/sccontacts.4f8e8942.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "978bba7865006727d4062805a9dd6920",
+    "url": "/static/media/taunggyi.978bba78.jpg"
+  }
+]);
